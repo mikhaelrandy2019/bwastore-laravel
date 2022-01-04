afe033195b2be07ca62ea425d0743de79ab992cc
mikhaelrandy2019 <mikhaelrandy@gmail.com>
2022-01-04 13:52:08 +0700

    Integrasi Dashboard My Product

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,99 +1,54 @@
 @extends('layouts.dashboard')
 
 @section('title')
-    Store Dashboard
+    Store Dashboard Product
 @endsection
 
 @section('content')
-<!-- Section Content -->
-<div
-class="section-content section-dashboard-home"
-data-aos="fade-up"
->
-<div class="container-fluid">
-    <div class="dashboard-heading">
-    <h2 class="dashboard-title">Dashboard</h2>
-    <p class="dashboard-subtitle">
-        Look what you have made today!
-    </p>
-    </div>
-    <div class="dashboard-content">
-    <div class="row">
-        <div class="col-md-4">
-        <div class="card mb-2">
-            <div class="card-body">
-            <div class="dashboard-card-title">
-                Customer
-            </div>
-            <div class="dashboard-card-subtitle">
-                {{ number_format($customer) }}
-            </div>
-            </div>
-        </div>
-        </div>
-        <div class="col-md-4">
-        <div class="card mb-2">
-            <div class="card-body">
-            <div class="dashboard-card-title">
-                Revenue
-            </div>
-            <div class="dashboard-card-subtitle">
-                ${{ number_format($revenue) }}
-            </div>
-            </div>
-        </div>
-        </div>
-        <div class="col-md-4">
-        <div class="card mb-2">
-            <div class="card-body">
-            <div class="dashboard-card-title">
-                Transaction
-            </div>
-            <div class="dashboard-card-subtitle">
-                {{ number_format($transaction_count) }}
-            </div>
-            </div>
-        </div>
-        </div>
-    </div>
-    <div class="row mt-3">
-        <div class="col-12 mt-2">
-            <h5 class="mb-3">Recent Transactions</h5>
-            @foreach ($transaction_data as $transaction)
-              <a
-                href="{{ route('dashboard-transaction-details', $transaction->id) }}"
-                class="card card-list d-block"
-                >
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-1">
-                            <img
-                                src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}"
-                                class="w-75"
-                            />
-                        </div>
-                        <div class="col-md-4">
-                                {{ $transaction->product->name ?? '' }}
-                            </div>
-                            <div class="col-md-3">
-                                {{ $transaction->transaction->user->name ?? '' }}
-                            </div>
-                            <div class="col-md-3">
-                                {{  $transaction->created_at ?? '' }}
-                            </div>
-                            <div class="col-md-1 d-none d-md-block">
-                                <img
-                                    src="/images/dashboard-arrow-right.svg"
-                                    alt=""
-                                />
-                        </div>
-                    </div>
+    <!-- Section Content -->
+          <div
+            class="section-content section-dashboard-home"
+            data-aos="fade-up"
+          >
+            <div class="container-fluid">
+              <div class="dashboard-heading">
+                <h2 class="dashboard-title">My Products</h2>
+                <p class="dashboard-subtitle">
+                  Manage it well and get money
+                </p>
+              </div>
+              <div class="dashboard-content">
+                <div class="row">
+                  <div class="col-12">
+                    <a
+                      href="{{ route('dashboard-product-create') }}"
+                      class="btn btn-success"
+                      >Add New Product</a
+                    >
+                  </div>
                 </div>
-            </a>  
-            @endforeach
-        </div>
-    </div>
-    </div>
-</div>
-</div>
+                <div class="row mt-4">
+
+                  @foreach ($products as $product)
+                      <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <a
+                          href="{{ route('dashboard-product-details', $product->id) }}"
+                          class="card card-dashboard-product d-block"
+                        >
+                          <div class="card-body">
+                            <img
+                              src="{{ Storage::url($product->galleries->first()->photos ?? '') }}"
+                              alt=""
+                              class="w-100 mb-2"
+                            />
+                            <div class="product-title">{{ $product->name }}</div>
+                            <div class="product-category">{{ $product->category->name }}</div>
+                          </div>
+                        </a>
+                      </div>
+                  @endforeach
+                </div>
+              </div>
+            </div>
+          </div>
 @endsection
